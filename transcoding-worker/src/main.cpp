cc9d654ef25fda7bdb7fd0431b8ea7b2b067b206
kofta <koftamainee@gmail.com>
2026-07-27 21:08:24 +0300

    fix formatting

--- transcoding-worker/src/main.cpp
+++ transcoding-worker/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "[transcoding-worker] started" << std::endl;
-    return 0;
+  std::cout << "[transcoding-worker] started" << std::endl;
+  return 0;
 }
